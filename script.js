1ebfa7466d659cab6fe41e908907c5a6dbcae028
zwemansoekun <45864942+zwemansoekun@users.noreply.github.com>
2024-01-18 05:03:17 +0630

    first upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $('#get_resume').click(function(e) {
+        e.preventDefault();  //stop the browser from following
+        window.location.href = 'upload/THIDA_HTAY_履　歴　書.xls';
+    });
+    
+});
